77a2a82abce7c6200c11339fd7d812438b2c861c
Ivan <iponomarev@mail.ru>
2024-06-23 10:27:50 +0100

    update jdbc example

--- jdbc-example/src/main/java/edu/phystech/jdbcdemo/Hello.java
+++ jdbc-example/src/main/java/edu/phystech/jdbcdemo/Hello.java
@@ -1,13 +1,23 @@
 package edu.phystech.jdbcdemo;
 
+import edu.phystech.jdbcdemo.domain.Conference;
+import edu.phystech.jdbcdemo.service.dao.ConferenceDao;
+import edu.phystech.jdbcdemo.service.db.SimpleJdbcTemplate;
+
+import javax.sql.DataSource;
 import java.io.IOException;
-import java.sql.*;
+import java.sql.SQLException;
 
 public class Hello {
 
 
-    public static void main(String[] args) throws SQLException, IOException {
-
+    public static void main(String[] args) throws IOException, SQLException {
+        DataSource ds = null; //From Hikari
+        final SimpleJdbcTemplate template = new SimpleJdbcTemplate(ds);
+        final ConferenceDao conferenceDao = new ConferenceDao(template);
+        for (Conference conference : conferenceDao.getConferences()) {
+            System.out.println(conference.getName());
+        }
     }
 
 }
