e9cbcd63c5ad4f490d886d003c79e95007e93274
Ivan Ponomarev <iponomarev@mail.ru>
2026-07-18 20:04:49 +0300

    remove unused throws (#27)

--- examples/springboot-example/src/main/java/edu/phystech/bootdemo/Main.java
+++ examples/springboot-example/src/main/java/edu/phystech/bootdemo/Main.java
@@ -8,7 +8,7 @@ import java.sql.SQLException;
 
 @SpringBootApplication
 public class Main {
-    public static void main(String[] args) throws SQLException, IOException {
+    public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
     }
 }
